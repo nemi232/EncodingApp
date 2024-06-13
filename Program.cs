2c4cf661dfbf5135a92c97cfc3b6baa3ccd25857
nemi232 <mkozlowska1003@gmail.com>
2024-06-13 15:23:44 +0200

    develop

--- Program.cs
+++ Program.cs
@@ -8,61 +8,73 @@ namespace Compress
     {
         static void Main(string[] args)
         {
-            string s = "4d";
+            string s = "40d";
             Console.WriteLine(RunLengthDecoding(s));
 
         }
 
-        public static string RunLengthDecoding(string s){
+        public static string RunLengthDecoding(string s)
+        {
             StringBuilder stringBuilder = new StringBuilder();
-            char [] charss = s.ToCharArray();
-            
+            char[] charss = s.ToCharArray();
 
-            for(int i = 0 ; i< charss.Length; i++)
+
+            for (int i = 0; i < charss.Length; i++)
             {
-                if(char.IsDigit(charss[i])){
-                    int count =0;
-                    i++;
-                    while(i < s.Length && char.IsDigit(charss[i]) ){
-                        stringBuilder.Append(charss[i]);
-                    }
-                }
                 
+               StringBuilder counter = new StringBuilder();
+                if (i + 1 != null)
+                {
+                    while (i < s.Length && char.IsDigit(charss[i]))
+                    {
+                        counter.Append(charss[i]);
+                    }
+
+                    stringBuilder.Append(counter);
+                }
                 
             }
             return stringBuilder.ToString();
 
+
         }
+            
+
+        
 
 
-        public static string RunLengthEncoding(string s){
-            StringBuilder stringBuilder = new StringBuilder();
-            char [] chars = s.ToCharArray();
-            int count =1;
+    public static string RunLengthEncoding(string s)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] chars = s.ToCharArray();
+        int count = 1;
 
-            for (int i = 1 ;i < chars.Length;i ++)
+        for (int i = 1; i < chars.Length; i++)
+        {
+            if (chars[i] == chars[i - 1])
             {
-                if(chars[i] == chars[i-1])
+                count++;
+            }
+            else
+            {
+                if (count != 1)
                 {
-                    count++;
+                    stringBuilder.Append(count.ToString());
+                    stringBuilder.Append(chars[i - 1]);
                 }
                 else
                 {
-                    if(count != 1){
-                    stringBuilder.Append(count.ToString());
-                    stringBuilder.Append(chars[i-1]);}
-                    else{
-                    stringBuilder.Append(chars[i-1]);
+                    stringBuilder.Append(chars[i - 1]);
                     stringBuilder.Append(chars[i]);
 
-                    }
-                    count =1;
                 }
-                
+                count = 1;
             }
 
-            return stringBuilder.ToString();
+        }
 
-        } 
+        return stringBuilder.ToString();
+
     }
+}
 }
