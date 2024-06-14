4be086c3516ccd8039d762d9393c4f76f28b0c82
nemi232 <mkozlowska1003@gmail.com>
2024-06-14 10:30:49 +0200

    new encode

--- Program.cs
+++ Program.cs
@@ -4,29 +4,62 @@ using System.Text;
 
 namespace Compress
 {
-    public class Compression : Decoderr
+    public class Compression
     {
 
         static void Main(string[] args)
         {
-            Decoderr c = new Compression();
 
-            string one = ">>>>.";
-            string two = "++++++++++...+<";
+            Console.WriteLine("Pick if you want to encode or decode. E/D");
+            string s = Console.ReadLine().ToLower();
+            if (s == "e" || s == "encode")
+            {
+                Console.WriteLine("Provide text to encoode ");
+                string en = Console.ReadLine();
+                Console.WriteLine($"Encoded output :  {RunLengthEncoding(en)}");
+            }
+            else
+            {
+                Console.WriteLine("Provide text to decode ");
+                string k = Console.ReadLine();
+                Console.WriteLine($"Decoded output :  {Decoderr.RunLengthDecoding(k)}");
+            }
 
-            string oneEncoded = Encode.RunLengthEncoding(one);
-            string twoEncoded = Encode.RunLengthEncoding(two);
-
-            Console.WriteLine(oneEncoded);
-            Console.WriteLine(twoEncoded);
-
-            // Console.WriteLine($"Encoded strings :  \n 1.  {one} is {oneEncoded} \n 2. {two} is {twoEncoded}");
-
-            // Console.WriteLine($"Decoded strings :  \n 1.  {oneEncoded} is {Decoderr.RunLengthDecoding(oneEncoded)} \n 2. {twoEncoded} is {Decoderr.RunLengthDecoding(twoEncoded)}");
 
 
         }
+        public static string RunLengthEncoding(string s)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int count = 1;
 
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i].Equals(s[i - 1]))
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count != 1)
+                    {
+                        stringBuilder.Append(count.ToString());
+                        stringBuilder.Append(s[i - 1]);
+                    }
+                    else
+                    {
+                        stringBuilder.Append(s[i]);
+                        
+                    }
+                    count = 1;
+                }
+
+
+            }
+
+            return stringBuilder.ToString();
+
+        }
 
 
     }
