19438bcc7ff86a0690b8b47c7bb54713249643d5
nemi232 <mkozlowska1003@gmail.com>
2024-06-14 11:09:14 +0200

    added console inpput

--- Encode.cs
+++ Encode.cs
@@ -4,31 +4,21 @@ using System.Text;
 namespace Compress{
     public class Encode{
 
-                        public static string RunLengthEncoding(string s)
+        public static string RunLengthEncoding(string s)
         {
             StringBuilder stringBuilder = new StringBuilder();
-            char[] chars = s.ToCharArray();
             int count = 1;
 
-            for (int i = 1; i < chars.Length; i++)
+            for (int i = 0; i < s.Length; i++)
             {
-                if (chars[i] == chars[i - 1])
+                if (i +1< s.Length && s[i] == s[i+1])
                 {
                     count++;
                 }
                 else
                 {
-                    if (count != 1)
-                    {
-                        stringBuilder.Append(count.ToString());
-                        stringBuilder.Append(chars[i - 1]);
-                    }
-                    else
-                    {
-                        stringBuilder.Append(chars[i - 1]);
-                        stringBuilder.Append(chars[i]);
-
-                    }
+                    stringBuilder.Append(count.ToString());
+                    stringBuilder.Append(s[i]);
                     count = 1;
                 }
 
@@ -38,5 +28,6 @@ namespace Compress{
 
         }
 
+        }
+
     }
-}
