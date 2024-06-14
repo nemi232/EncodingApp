077a0ea0169390055323ea64ef61fc539cf8e915
nemi232 <mkozlowska1003@gmail.com>
2024-06-14 08:59:53 +0200

    finished classes

---
+++ Decode.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Text;
+
+namespace Compress{
+    interface Decoderr
+    {
+        public static string RunLengthDecoding(string s)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int length = s.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    int count = 0;
+                    while (i < length && char.IsDigit(s[i]))
+                    {
+                        int secondDigit = s[i] - '0';
+                        count = count * 10 + secondDigit;
+                        i++;
+                    }
+
+                    for (int j = 0; j < count; j++)
+                    {
+                        stringBuilder.Append(s[i]);
+                    }
+                }
+                else
+                {
+                    stringBuilder.Append(s[i]);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,77 +4,32 @@ using System.Text;
 
 namespace Compress
 {
-    public class Compression
+    public class Compression : Decoderr
     {
+
         static void Main(string[] args)
         {
-            string s = "40d";
-            Console.WriteLine(RunLengthDecoding(s));
+            Decoderr c = new Compression();
 
-        }
+            string one = ">>>>.";
+            string two = "++++++++++...+<";
 
-        public static string RunLengthDecoding(string s)
-        {
-            StringBuilder stringBuilder = new StringBuilder();
-            char[] charss = s.ToCharArray();
+            string oneEncoded = Encode.RunLengthEncoding(one);
+            string twoEncoded = Encode.RunLengthEncoding(two);
 
+            Console.WriteLine(oneEncoded);
+            Console.WriteLine(twoEncoded);
 
-            for (int i = 0; i < charss.Length; i++)
-            {
-                
-               StringBuilder counter = new StringBuilder();
-                if (i + 1 != null)
-                {
-                    while (i < s.Length && char.IsDigit(charss[i]))
-                    {
-                        counter.Append(charss[i]);
-                    }
+            // Console.WriteLine($"Encoded strings :  \n 1.  {one} is {oneEncoded} \n 2. {two} is {twoEncoded}");
 
-                    stringBuilder.Append(counter);
-                }
-                
-            }
-            return stringBuilder.ToString();
+            // Console.WriteLine($"Decoded strings :  \n 1.  {oneEncoded} is {Decoderr.RunLengthDecoding(oneEncoded)} \n 2. {twoEncoded} is {Decoderr.RunLengthDecoding(twoEncoded)}");
 
 
         }
-            
-
-        
 
 
-    public static string RunLengthEncoding(string s)
-    {
-        StringBuilder stringBuilder = new StringBuilder();
-        char[] chars = s.ToCharArray();
-        int count = 1;
-
-        for (int i = 1; i < chars.Length; i++)
-        {
-            if (chars[i] == chars[i - 1])
-            {
-                count++;
-            }
-            else
-            {
-                if (count != 1)
-                {
-                    stringBuilder.Append(count.ToString());
-                    stringBuilder.Append(chars[i - 1]);
-                }
-                else
-                {
-                    stringBuilder.Append(chars[i - 1]);
-                    stringBuilder.Append(chars[i]);
-
-                }
-                count = 1;
-            }
-
-        }
-
-        return stringBuilder.ToString();
 
     }
-}
+
+
 }
